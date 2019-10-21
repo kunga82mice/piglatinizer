e5ffdfe0cbe637eec6290ac472a4163123b1380a
kunga82mice <kunga82mice@gmail.com>
2019-10-21 22:07:02 +0000

    10/21/2019. Screen is not pink.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+function pig(str)
+{
+    return str;
+}
+
+  $("button").click(() => {
+      let message = $("input").val();
+      $("body").append(message);
+  })
 
 
-  
-  
-  
-  
 });
