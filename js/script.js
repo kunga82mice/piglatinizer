5a02eac52cbf75093dd61e203d62c4d0a94fbb8f
kunga82mice <kunga82mice@gmail.com>
2019-10-28 22:06:37 +0000

    10_28_19 corrected sum bug

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
+$(".output").text("hey");
+var nums= $("input").val();
+var nums_array = nums.split('+');
 
 function pig(str)
 {
